adc612e70fa2a80418945ea3acd9267cda0467a2
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-07 14:29:53 -0300

    Interface Cliente creation

---
+++ src/app/models/cliente.ts
@@ -0,0 +1,9 @@
+export interface Cliente {
+  id: number;
+  nome: string;
+  cpf: string;
+  dataNascimento: string;
+  rendaMensal: number;
+  email: string;
+  dataCadastro: string;
+}
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,34 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
+import { Cliente } from '../models/cliente';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClienteService {
-  private aprUrl = ' http://localhost:3000/clientes';
+  private aprUrl = 'http://localhost:3000/clientes';
 
   constructor(private http: HttpClient) {}
 
-  getAll(): Observable<any> {
-    return this.http.get<any>(this.aprUrl);
+  getAll(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.aprUrl);
   }
 
-  getById(idCliente: number): Observable<any> {
+  getById(idCliente: number): Observable<Cliente | null> {
     return this.getAll().pipe(
-      map((data) => {
-        const clientes = data.clientes || [];
-        const cliente = clientes.find((c: any) => c.id === idCliente);
+      map((clientes: Cliente[]) => {
+        const cliente = clientes.find((c: Cliente) => c.id === idCliente);
         return cliente || null;
       })
     );
   }
 
-  update(cliente: any): Observable<any> {
+  update(cliente: Cliente): Observable<any> {
     return this.http.put(this.aprUrl, cliente);
   }
 
-  save(cliente: any): Observable<any> {
+  save(cliente: Cliente): Observable<Cliente> {
     return this.http.post<any>(this.aprUrl, cliente);
   }
 
