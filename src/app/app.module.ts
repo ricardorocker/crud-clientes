38196f2fb404fe207056f284f8f969c37bcd50ca
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-07 06:55:40 -0300

    Client form page creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListagemComponent } from './views/clientes/listagem/listagem.component';
+import { FormularioComponent } from './views/clientes/formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ListagemComponent
+    ListagemComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
