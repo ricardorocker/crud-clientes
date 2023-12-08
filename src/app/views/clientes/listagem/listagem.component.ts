20eaa3513ea18795fa11072831d96897f42e123c
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-08 16:02:01 -0300

    Table: ngFor(mockData) and ngClass(row even/odd)

--- src/app/views/clientes/listagem/listagem.component.ts
+++ src/app/views/clientes/listagem/listagem.component.ts
@@ -7,6 +7,27 @@ import { Router } from '@angular/router';
   styleUrls: ['./listagem.component.scss'],
 })
 export class ListagemComponent {
+  clientes = [
+    {
+      id: 1,
+      nome: 'Ricardo Santos Rocker',
+      cpf: '442.672.838-00',
+      dataNascimento: '04/09/1995',
+      rendaMensal: 24400,
+      email: 'ricardo.santos.rocker@gmail.com',
+      dataCadastro: '08/12/2023',
+    },
+    {
+      id: 2,
+      nome: 'Petrucia Lira Soares',
+      cpf: '121.672.458-00',
+      dataNascimento: '18/10/1997',
+      rendaMensal: 17400,
+      email: 'lira.petrucia@gmail.com',
+      dataCadastro: '09/12/2023',
+    },
+  ];
+
   constructor(private router: Router) {}
 
   redirect(): void {
