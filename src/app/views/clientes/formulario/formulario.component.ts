60a2c182371c6c3191e5129b53acee493d3fb9f0
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-08 09:36:52 -0300

    idadeValidation as ValidatorFn

--- src/app/views/clientes/formulario/formulario.component.ts
+++ src/app/views/clientes/formulario/formulario.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgxMaskDirective } from 'ngx-mask';
 import { SobrenomeValidatorPipe } from 'src/app/pipes/sobrenome-validator.pipe';
 import { CpfValidatorPipe } from 'src/app/pipes/cpf-validator.pipe';
+import { idadeValidator } from 'src/app/validators/idade.validator';
 
 @Component({
   selector: 'app-formulario',
@@ -13,12 +13,13 @@ import { CpfValidatorPipe } from 'src/app/pipes/cpf-validator.pipe';
 })
 export class FormularioComponent {
   form: FormGroup;
+  maskDataNascimento = "/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/";
 
   constructor(private router: Router, private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
       nome: ['', Validators.required],
       cpf: ['', Validators.required],
-      dataNascimento: ['', Validators.required],
+      dataNascimento: ['', [Validators.required, idadeValidator()]],
       rendaMensal: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       dataCadastro: ['', Validators.required],
